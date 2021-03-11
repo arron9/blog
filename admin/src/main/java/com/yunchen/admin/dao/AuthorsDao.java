dd74a45b4902660da0573b7cb298f9d74e03a237
shuangxijiang <594156739@qq.com>
2021-03-11 17:13:12 +0800

    feat(api模块): 增加api前台接口

--- admin/src/main/java/com/yunchen/admin/dao/AuthorsDao.java
+++ admin/src/main/java/com/yunchen/admin/dao/AuthorsDao.java
@@ -1,5 +1,6 @@
 package com.yunchen.admin.dao;
 
+import com.yunchen.common.dao.BaseDao;
 import com.yunchen.common.model.Authors;
 public interface AuthorsDao extends BaseDao<Authors, Integer> {
 }
