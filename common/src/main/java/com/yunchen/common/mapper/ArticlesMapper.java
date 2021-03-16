4953e38b4aabc4c0b6dc0ec18e9e7ff8f841f653
shuangxijiang <594156739@qq.com>
2021-03-16 16:30:13 +0800

    feat(文章): 文章列表接口

--- common/src/main/java/com/yunchen/common/mapper/ArticlesMapper.java
+++ common/src/main/java/com/yunchen/common/mapper/ArticlesMapper.java
@@ -3,5 +3,8 @@ package com.yunchen.common.mapper;
 import com.yunchen.common.model.Articles;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+
 public interface ArticlesMapper extends Mapper<Articles> {
+    List<Articles> listArticles(Articles articles);
 }
